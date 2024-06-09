1c82c7a1a43e18522b494ada96ae810c95fe4a55
takuto <tiharu717@gmail.com>
2024-06-09 14:28:23 +0900

    途中まで

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:sinsetu_prototype/timer.dart';
 
@@ -6,6 +7,14 @@ import 'package:sinsetu_prototype/timer.dart';
 const notifierMessage = "こんにちは";
 const notifierTitle = "タイトル";
 const notifierSeconds = 10;
+
+const double widgetPaddingSize = 15;
+const TextStyle titleTextStyle = TextStyle(
+  decoration: TextDecoration.underline,
+  fontStyle: FontStyle.italic,
+  fontFamily: "Zapfino",
+  fontSize: 25,
+);
 
 final flutterLocalNotificationPlugin = FlutterLocalNotificationsPlugin();
 final DarwinInitializationSettings initializationSettingsDarwin = DarwinInitializationSettings(
@@ -46,10 +55,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple), useMaterial3: true),
       home: const MyHomePage(
         title: "アプリ",
       ),
@@ -73,24 +79,22 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        primary: false,
       ),
-      body: Stack(
-        children: [
-          TextButton(
-            onPressed: () {
-              setState(() {});
-            },
-            child: const SizedBox.shrink(),
-          ),
-          const Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[],
-            ),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: LayoutBuilder(builder: (context, constraints) {
+          return Column(
+            children: [
+              talkingWith(),
+              const SizedBox(
+                height: 30,
+              ),
+              lastTalking(),
+              tuuwaButtons(),
+            ],
+          );
+        }),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
@@ -100,6 +104,194 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  final widgetDecoration = BoxDecoration(borderRadius: BorderRadius.circular(40), color: Colors.grey[200]);
+
+  Widget talkingWith() {
+    return Container(
+      decoration: widgetDecoration,
+      child: const Padding(
+        padding: EdgeInsets.all(widgetPaddingSize),
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Text(
+                "Talking with ...",
+                style: titleTextStyle,
+                textAlign: TextAlign.start,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "@sinsetu_prototype",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Text(
+              "あなたと同じ番組を見ていました",
+              style: TextStyle(fontSize: 15),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget lastTalking() {
+    return Container(
+        decoration: widgetDecoration,
+        child: Padding(
+          padding: const EdgeInsets.all(widgetPaddingSize),
+          child: Column(
+            children: [
+              const SizedBox(
+                width: double.infinity,
+                child: Text(
+                  "Last talking",
+                  style: titleTextStyle,
+                  textAlign: TextAlign.start,
+                ),
+              ),
+              lastTalkingDate(),
+              const SizedBox(
+                height: 10,
+              ),
+              keywordsWidget(),
+            ],
+          ),
+        ));
+  }
+
+  Widget lastTalkingDate() {
+    return const Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          Text(
+            "Two months ago...",
+            style: TextStyle(fontSize: 30),
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: Text(
+              "2024/04/22",
+              textAlign: TextAlign.end,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget keywordsWidget() {
+    const keywords = [
+      "結婚",
+      "子供",
+      "親戚",
+    ];
+    return Column(
+      children: [
+        const SizedBox(
+          child: Text(
+            "Keywords",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            for (final keyword in keywords)
+              Card(
+                  margin: const EdgeInsets.all(15),
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(vertical: 7, horizontal: 13),
+                    child: Text(
+                      keyword,
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                  )),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget tuuwaButtons() {
+    final buttonWaku = BoxDecoration(border: Border.all(color: Colors.black), borderRadius: BorderRadius.circular(200));
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(200)),
+                      child: const Icon(
+                        size: 40,
+                        Icons.volume_up,
+                      ),
+                    ),
+                    const Text(
+                      "スピーカー\nをオン",
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(200)),
+                      child: Icon(
+                        size: 40,
+                        Icons.mic_off_outlined,
+                      ),
+                    ),
+                    const Text("マイクをオフ"),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(200), color: Colors.red),
+                child: const Padding(
+                  padding: EdgeInsets.all(18.0),
+                  child: Icon(
+                    color: Colors.white,
+                    Icons.close,
+                    size: 40,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
