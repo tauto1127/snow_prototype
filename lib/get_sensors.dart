3942aa6071ecb5c7766fe995ca2e7229972dc8ce
Ruri-Kinoshita <b1023081@fun.ac.jp>
2024-06-12 19:19:19 +0900

    コンパスを作ったけど，別にいらないと気づく

--- lib/get_sensors.dart
+++ lib/get_sensors.dart
@@ -1,38 +1,23 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_compass/flutter_compass.dart';
 import 'package:geolocator/geolocator.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Compass Flutter',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Compass(),
-    );
-  }
-}
+import 'package:sinsetu_prototype/main.dart';
 
 class Compass extends StatefulWidget {
   const Compass({super.key});
-
   @override
   _CompassState createState() => _CompassState();
 }
 
 class _CompassState extends State<Compass> {
+  @override
   Position myPosition = Position(
+    altitudeAccuracy: 0,
+    headingAccuracy: 0,
     latitude: 41.84212,
     longitude: 140.76847,
     timestamp: DateTime.now(),
@@ -42,49 +27,40 @@ class _CompassState extends State<Compass> {
     speed: 0,
     speedAccuracy: 0,
     floor: null,
-    altitudeAccuracy: 0,
-    headingAccuracy: 0,
   );
-
   late StreamSubscription<Position> myPositionStream;
   late double? deviceDirection;
   final LocationSettings locationSettings = const LocationSettings(
     accuracy: LocationAccuracy.high,
     distanceFilter: 10,
   );
-
   Position markerPosition = Position(
-    latitude: 35.63485362875449,
-    longitude: 139.88524000230558,
-    timestamp: DateTime.now(),
-    altitude: 0,
-    accuracy: 0,
-    heading: 0,
-    speed: 0,
-    speedAccuracy: 0,
-    floor: null,
-    altitudeAccuracy: 0,
-    headingAccuracy: 0,
-  );
-
+      longitude: 41.767470763313604,
+      latitude: 140.73334598482998,
+      timestamp: DateTime.now(),
+      accuracy: 0,
+      altitude: 0,
+      altitudeAccuracy: 0,
+      heading: 0,
+      headingAccuracy: 0,
+      speed: 0,
+      speedAccuracy: 0);
   late double markerDirection;
   double directionTolerance = 5.0;
 
-  // Function to calculate direction from startPosition to endPosition
   double calcDirection(Position startPosition, Position endPosition) {
-    double direction = Geolocator.bearingBetween(
-      startPosition.latitude,
-      startPosition.longitude,
-      endPosition.latitude,
-      endPosition.longitude,
-    );
+    double startLat = startPosition.latitude;
+    double startLng = startPosition.longitude;
+    double endLat = endPosition.latitude;
+    double endLng = endPosition.longitude;
+    double direction =
+        Geolocator.bearingBetween(startLat, startLng, endLat, endLng);
     if (direction < 0.0) {
       direction += 360.0;
     }
     return direction;
   }
 
-  // Function to check if the directions are within tolerance
   bool checkTolerance(double direction1, double direction2) {
     if ((direction1 - direction2).abs() < directionTolerance) {
       return true;
@@ -99,7 +75,7 @@ class _CompassState extends State<Compass> {
   void initState() {
     super.initState();
 
-    // Request location permissions if not already granted
+    // 位置情報サービスが許可されていない場合は許可をリクエストする
     Future(() async {
       LocationPermission permission = await Geolocator.checkPermission();
       if (permission == LocationPermission.denied) {
@@ -107,29 +83,22 @@ class _CompassState extends State<Compass> {
       }
     });
 
-    // Start listening to the user's position updates
+    // ユーザの現在位置を取得し続ける
     myPositionStream =
         Geolocator.getPositionStream(locationSettings: locationSettings)
             .listen((Position position) {
-      setState(() {
-        myPosition = position;
-      });
+      myPosition = position;
     });
   }
 
   @override
   void dispose() {
-    myPositionStream
-        .cancel(); // Ensure to cancel the stream when disposing the widget
+    myPositionStream.cancel();
     super.dispose();
   }
 
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Compass Flutter'),
-      ),
       body: StreamBuilder<CompassEvent>(
         stream: FlutterCompass.events,
         builder: (context, snapshot) {
@@ -141,26 +110,44 @@ class _CompassState extends State<Compass> {
               child: CircularProgressIndicator(),
             );
           }
-
           deviceDirection = snapshot.data?.heading;
           if (deviceDirection == null) {
             return const Center(
-              child: Text("Device does not have sensors."),
+              child: Text("Device does not have sensors !"),
             );
           }
-
           markerDirection = calcDirection(myPosition, markerPosition);
 
           return Center(
-            child: Opacity(
-              opacity:
-                  checkTolerance(deviceDirection!, markerDirection) ? 1.0 : 0.0,
-              child: const Icon(
-                Icons.expand_less,
-                size: 100,
+              child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(width: 2.0, color: Colors.black),
+                ),
               ),
-            ),
-          );
+              Transform.rotate(
+                angle: (deviceDirection! * (3.141592653589793 / 180) * -1),
+                child: Icon(
+                  Icons.navigation_outlined,
+                  size: 100,
+                  color: checkTolerance(deviceDirection!, markerDirection)
+                      ? Colors.red
+                      : Colors.blue,
+                ),
+              )
+            ],
+          )
+
+              // Text(
+              // '${direction.toStringAsFixed(0)}°',
+              // style: const TextStyle(fontSize: 100),
+              // ),
+              );
         },
       ),
     );
